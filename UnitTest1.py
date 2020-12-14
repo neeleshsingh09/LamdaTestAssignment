46bcf2dca31cf03c9558c6c359fa7bc9f679e3b9
Neelesh <neeleshsingh1987@gmail.com>
2020-12-14 16:56:40 +0100

    Test

--- UnitTest1.py
+++ UnitTest1.py
@@ -78,42 +78,67 @@ class LTAutomate(unittest.TestCase):
         self.driver.get("https://www.lambdatest.com/automation-demos/")
         self.driver.maximize_window()
 
+        self.driver.get("https://www.lambdatest.com/automation-demos/")
         element1 = self.driver.find_element_by_css_selector("*[class='cbtn btn_accept_ck']")
         if element1.is_displayed():
             element1.click()
+
         self.driver.find_element_by_name("name").send_keys("lambda")
         self.driver.find_element_by_name("password").send_keys("lambda123")
-        time.sleep(3)
 
         element = self.driver.find_element_by_css_selector("[class='applynow']")
+
         actions = ActionChains(self.driver)
         actions.move_to_element(element).perform()
         element.click()
-
-        # self.driver.find_element_by_css_selector("[class='applynow']").click()
         # self.driver.find_element_by_xpath("//*[@id='applyform']/div/button").click()
 
         self.driver.find_element_by_name("email").send_keys("neeleshsingh1987@gmail.com")
         self.driver.find_element_by_id("populate").click()
-        time.sleep(5)
+        time.sleep(3)
 
         alert_obj = self.driver.switch_to.alert
         alert_obj.accept()
+        time.sleep(5)
 
+        self.driver.find_element_by_id("month").click()
+        self.driver.find_element_by_id("customer-service").click()
+        # self.driver.find_element_by_id("preferred-payment").select_by_value('Wallets')
+        self.driver.find_element_by_xpath("//select[@name='preferred-payment']/option[text()='Wallets']").click()
+        self.driver.find_element_by_id("tried-ecom").click()
+        time.sleep(3)
 
+        self.driver.find_element_by_css_selector("*[id='slider'] + div > div:nth-child(9)").click()
+        time.sleep(3)
 
-        print("Printing title of current page :" + driver.title)
-        driver.execute_script("lambda-status=passed")
+        # Open a new window
+        self.driver.execute_script("window.open('');")
+        # Switch to the new window
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        self.driver.get("https://www.lambdatest.com/selenium-automation")
+
+        time.sleep(3)
+
+        # Get image source
+        image = self.driver.find_element_by_css_selector("img[src *= 'jenkins.svg']")
+        src = image.get_attribute('src')
+        self.driver.get('src')
+        self.driver.save_screenshot('screenshot.png')
+
+        print("Printing title of current page :" + self.driver.title)
+        self.driver.execute_script("lambda-status=passed")
         print("Requesting to mark test : pass")
 
-    """
-    Quit selenium driver
-    """
 
-    def tearDown(self):
-        self.driver.close()
-        self.driver.quit()
-        print("Test completed")
+"""
+Quit selenium driver
+"""
+
+
+def tearDown(self):
+    self.driver.close()
+    self.driver.quit()
+    print("Test completed")
 
 
 if __name__ == "__main__":
